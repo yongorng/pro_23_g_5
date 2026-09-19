bedd08b8bd7f451c5e6251e3703ecc99a70b9d0d
yongorng <yongorng@gmail.com>
2026-09-19 22:20:35 +0700

    refactor: add centralized app pages

---
+++ lib/route/app_page.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+
+import '../screen/auth/login_screen.dart';
+import '../screen/auth/register_screen.dart';
+import '../screen/main_screen.dart';
+import 'app_route.dart';
+
+class AppPage {
+  const AppPage._();
+
+  static final List<GetPage<dynamic>> pages = [
+    GetPage<dynamic>(
+      name: AppRoute.login,
+      page: () => const LoginScreen(),
+    ),
+    GetPage<dynamic>(
+      name: AppRoute.register,
+      page: () => const RegisterScreen(),
+    ),
+    GetPage<dynamic>(
+      name: AppRoute.main,
+      page: () => const MainScreen(),
+    ),
+  ];
+}
